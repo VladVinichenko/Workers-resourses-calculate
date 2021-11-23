8b439ee0dcb336012efb3c7e2d79eccdfc954032
VladVinichenko <vinnyxxxl925@gmail.com>
2021-11-23 11:27:51 +0200

    ffd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import './sass/main.scss';
+import './js/refs/refs';
+import './js/main';
+import './js/mine';
---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+import mine_markup from '../partials/hbs/mine.hbs';
+import refs from './refs/refs';
+
+refs.main.insertAdjacentHTML('beforeend', mine_markup());
